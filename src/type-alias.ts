e076424563d0f3e5f3f5595f90df1096b4506423
mustofa adny <adniadni04@gmail.com>
2024-07-09 21:54:14 +0700

    type-alias

--- src/type-alias.ts
+++ src/type-alias.ts
@@ -1,10 +1,11 @@
+export type ID = string | number;
 export type Category = {
-  id: string;
+  id: ID;
   name: string;
 };
 
 export type Product = {
-  id: string;
+  id: ID;
   name: string;
   price: number;
   category: Category;
