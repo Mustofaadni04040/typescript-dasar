fa1838a9a957953c14559ddcdda2c8958a90e5ad
mustofa adny <adniadni04@gmail.com>
2024-07-09 21:47:32 +0700

    first commit

---
+++ tests/type-alias.test.ts
@@ -0,0 +1,20 @@
+import { Category, Product } from "../src/type-alias";
+
+describe("Type Alias", function () {
+  it("should support in typescript", function () {
+    const category: Category = {
+      id: "1",
+      name: "Mustofa",
+    };
+
+    const product: Product = {
+      id: "1",
+      name: "Samsung S20",
+      price: 100000,
+      category,
+    };
+
+    console.info(category);
+    console.info(product);
+  });
+});
