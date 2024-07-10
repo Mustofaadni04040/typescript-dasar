7a7f88e5a74bc99e792466c0a257e5841b4cb7be
mustofa adny <adniadni04@gmail.com>
2024-07-10 21:06:11 +0700

    extending interface

--- src/seller.ts
+++ src/seller.ts
@@ -2,4 +2,6 @@ export interface Seller {
   id: number;
   name: string;
   addrees?: string;
+  readonly nib: string;
+  readonly npwp: string;
 }
---
+++ tests/interface.test.ts
@@ -0,0 +1,71 @@
+import { Employee, Manager } from "../src/employee";
+import { Seller } from "../src/seller";
+
+describe("Interface", function () {
+  it("should support in typescript", function () {
+    const seller: Seller = {
+      id: 1,
+      name: "Mustofa Adny",
+      nib: "1234567890",
+      npwp: "1234567890",
+    };
+    console.info(seller);
+  });
+
+  //function interface
+  it("should support function interface", function () {
+    interface addFunction {
+      (value1: number, value2: number): number;
+    }
+
+    const add: addFunction = (value1: number, value2: number): number => {
+      return value1 + value2;
+    };
+
+    expect(add(2, 2)).toBe(4);
+  });
+
+  // indexable interface
+  it("support indexable interface", function () {
+    interface StringArray {
+      [index: number]: string;
+    }
+
+    const names: StringArray = ["Mustofa", "Adny"];
+
+    expect(names[0]).toBe("Mustofa");
+    console.info(names);
+  });
+
+  it("support indexable interface for non number index", function () {
+    interface StringDictionary {
+      [key: string]: string;
+    }
+
+    const dictionary: StringDictionary = {
+      names: "Mustofa",
+      address: "Indonesia",
+    };
+
+    expect(dictionary["names"]).toBe("Mustofa");
+    expect(dictionary["address"]).toBe("Indonesia");
+  });
+
+  // extending interface : melanjutkan ke interface lainnya
+  it("should support extend interface", function () {
+    const employee: Employee = {
+      id: "1",
+      name: "Mustofa",
+      division: "IT",
+    };
+    console.info(employee);
+
+    const manager: Manager = {
+      id: "1",
+      name: "Adny",
+      division: "IT",
+      numberOfEmployees: 10,
+    };
+    console.info(manager);
+  });
+});
